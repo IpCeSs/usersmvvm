5521bc5f6d9f08093c44bbc399a7e4e035bf0669
Cecilia <ceciliapigeolet@gmail.com>
2018-06-29 10:06:13 +0200

    Ajoutez des fichiers projet.

---
+++ UserMVVM/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserMVVM.ViewModel;
+
+namespace UserMVVM
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserViewModel _userViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _userViewModel = new UserViewModel();
+            base.DataContext = _userViewModel;
+
+         
+        }
+    }
+}
---
+++ UserMVVM/ViewModel/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserMVVM.Model;
+
+namespace UserMVVM.ViewModel
+{
+    class UserViewModel
+    {
+        public List<UserModel> Users { get; set; }
+
+        public UserViewModel()
+        {
+            LoadUsers();
+
+        }
+        public void LoadUsers()
+        {
+            List<UserModel> usersList = new List<UserModel>
+            {
+                new UserModel { FirstName = "Vincent", LastName = "Fourberie" },
+                new UserModel { FirstName = "Arnaud", LastName = "Papote" },
+                new UserModel { FirstName = "Sandrine", LastName = "Trahison" }
+            };
+
+            Users = usersList;
+
+        }
+    }
+}
